1e12a5fbba393915505cae7311558bb9b33ea39a
louwailou <sun_224@sina.com>
2016-12-21 22:47:52 +0800

    添加 ignore

--- Gomoku/ViewController.swift
+++ Gomoku/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let (data, enc) = UTF8ToGB2312(str: "cbefbda8b7e5")
+        let gbkStr = NSString(data: data as! Data, encoding: enc)!
+        
+        print("GBK string is: \(gbkStr)")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +23,15 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func UTF8ToGB2312(str: String) -> (NSData?, UInt) {
+        let enc = CFStringConvertEncodingToNSStringEncoding(UInt32(CFStringEncodings.GB_18030_2000.rawValue))
+        let datas = str.data(using: String.Encoding(rawValue: enc), allowLossyConversion: false)
+        
+        
+        return (datas as NSData?, enc)
+    }
 
 }
+
+
 
---
+++ Podfile
@@ -0,0 +1,14 @@
+
+platform :ios, '8.0'
+
+inhibit_all_warnings!
+use_frameworks!
+
+target ‘Gomoku’ do
+    
+    pod 'RxSwift',    '~> 3.0'
+    pod 'RxCocoa',    '~> 3.0'
+
+end
+
+
